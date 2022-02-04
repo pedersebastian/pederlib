931c4efc0e37a3f90da0bcaa4af6826bd170039f
Peder Sebastian Martinsen <peder.s.martinsen@gmail.com>
2022-02-04 22:05:39 +0100

    rm discrim

--- R/otherfunctions.R
+++ R/otherfunctions.R
@@ -1,18 +1,3 @@
-utils::globalVariables(c("%>%" ,".metric" ,"aes", "annotate" ,"arrange", "as_tibble" ,"bind_rows", "case_when",
-                         "collect_metrics", "element_text" ,"facet_wrap", "geom_count", "geom_errorbar",
-                         "geom_hline" ,"geom_point", "geom_vline", "ggplot", "ggtitle" ,"group_by" ,"labs",
-                         "latitude", "longitude", "metric_name", "model", "mutate", "n" ,"new_geo", "pivot_wider",
-                         "read_xml" ,"recipe" ,"row_number" ,"scale_x_continuous", "scale_y_continuous",
-                         "separate", "separate_rows" ,"slice_max", "slice_min", "std_err", "str_detect",
-                         "str_remove_all", "str_replace_all" ,"str_squish", "str_to_upper" ,"theme",
-                         "theme_set", "type", "ungroup", "update_geom_defaults", "value", "vars" ,"wflow_id",
-                         "workflow_rank" ,"xlab" ,"ylab"))
-
-
-
-#S3method(print, myClass)
-
-
 
 ### extract latitude and longitude for geo_field like GEO3212(31.3112,-88.22)
 parse_geo <- function(tbl, geo_variable) {
@@ -55,7 +40,6 @@ startup <- function(type = 1, paralell = TRUE) {
   }
   else{
   suppressPackageStartupMessages(library(baguette))
- # suppressPackageStartupMessages(library(discrim))
   suppressPackageStartupMessages(library(tidymodels))
   suppressPackageStartupMessages(library(tidyverse))
   suppressPackageStartupMessages(library(finetune))
@@ -63,7 +47,7 @@ startup <- function(type = 1, paralell = TRUE) {
   suppressPackageStartupMessages(library(stacks))
   suppressPackageStartupMessages(library(lubridate))
   suppressPackageStartupMessages(library(themis))
-  mes <- " Tidymodels, Tidyverse, Finetune, Themis, Lubridate, Textrecipes and Stacks has been loaded.\n Baguette, Discrim commented out"
+  mes <- "Tidymodels, Tidyverse, Finetune, Baguette, Themis, Lubridate, Textrecipes and Stacks has been loaded."
   }
 
   if (paralell) {
